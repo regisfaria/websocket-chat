9affd3505cffc2cbdaafcffb6f0665cae1c9abdd
regisfaria <rgisfaria@yahoo.com.br>
2021-07-24 21:04:52 -0300

    create user service

--- src/http.ts
+++ src/http.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+
 import path from 'path';
 import express from 'express';
 import mongoose from 'mongoose';
--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,7 +1,22 @@
+import { container } from 'tsyringe';
+
 import { websocket } from '../http';
 
+import { CreateUserService } from '../services/CreateUserService';
+
 websocket.on('connect', socket => {
-  socket.emit('chat_started', {
-    message: "Your chat was started",
+  socket.on('start', async data => {
+    const { email, avatar, name } = data;
+
+    const createUserService = container.resolve(CreateUserService);
+
+    const user = await createUserService.execute({
+      email,
+      socket_id: socket.id,
+      avatar,
+      name,
+    });
+
+    console.log(user);
   });
 });
